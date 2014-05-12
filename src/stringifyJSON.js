b07abc296822e465968c3e1d2758c742b2048887
Jasen Lew <jasen.lew@gmail.com>
2014-05-12 11:20:09 -0700

    stringifyJSON - passed first two tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,54 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  if (typeof obj === 'number') {
+  	return obj.toString();
+  } else if (typeof obj === 'boolean') {
+  	return obj.toString();  	
+  } else if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  } else if (typeof obj === 'undefined') {
+  	return "undefined";
+  } else if (obj === null) {
+  	return "null";
+  } else if (Object.prototype.toString.call(obj) === '[object Array]' || Object.prototype.toString.call(obj) === '[object Object]') {
+ 
+  	var str = '';
+
+  	if (Object.prototype.toString.call(obj) === '[object Array]') {
+	  	str += '[';
+	  	
+	  	for (var i = 0, l = obj.length; i < l; i += 1) {
+	  		str += stringifyJSON(obj[i]);
+	  		if (i + 1 < l) {
+	  			str += ',';
+	  		}
+	  	}
+	  	str += ']';
+  	}
+
+  	if (Object.prototype.toString.call(obj) === '[object Object]') {
+		str += '{';
+		var objectLength = _.size(obj);
+		var counter = 1;
+		
+		for (var key in obj) {
+		  if (obj.hasOwnProperty(key)) {
+		    str += stringifyJSON(key);
+		    str += ':';
+		    str += stringifyJSON(obj[key]);
+		  }
+		  if (counter < objectLength) {
+		  	str += ',';
+		  }
+		  counter += 1;
+		}
+	  	str += '}';
+	}
+  	return str;
+  }
 };
+
+stringifyJSON(["apple", true, {"foo": true}]);
+// "["apple", true, {"foo": true}]"
